8a5f8a1fdc0f55851760401261ecb66c13f67d91
3dstepansky <3dstepansky@gmail.com>
2025-12-04 03:13:45 +0300

    Delete checker (1).js

--- checker (1).js
+++
@@ -1,9 +0,0 @@
-import express from express;
-import { TelegramClient } from telegram;
-import { StringSession } from telegram/sessions/index.js;
-import { Api } from telegram;
-
-/** --- simple auth --- */
-function isAuth(req) {
-  const h = req.headers[authorization] || ;
-  const token = h.startsWith(Bearer
